56ba0d1ed9366677595ab82c255c57e22356c473
João Carlos <joao@etecia.com.br>
2020-06-03 20:28:21 -0300

    estrura galeria e cadastro

---
+++ cadastrar.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>Clorocine</title>
+</head>
+
+<body>
+
+    <nav class="nav-extended purple lighten-3">
+        <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="cadastrar.php">Cadastrar</a></li>
+            </ul>
+        </div>
+        <div class="nav-header center">
+            <h1>CLOROCINE</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent purple darken-1">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                <li class="tab"><a href="#test2">Assistidos</a></li>
+                <li class="tab"><a href="#test3">Favoritos</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="row">
+        <div class="col s6 offset-s3">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title">Cadastrar Filme</span>
+
+                    <!-- input titulo-->
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="titulo" type="text" class="validate" require>
+                            <label for="titulo">Título do Filme</label>
+                        </div>
+                    </div>
+
+                    <!-- input sinopse-->
+                    <div class="row">
+                        <form class="col s12">
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <textarea id="sinopse" class="materialize-textarea"></textarea>
+                                    <label for="sinopse">Sinopse</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!-- input nota-->
+                    <div class="row">
+                        <div class="input-field col s4">
+                            <input id="nota" type="number" step=".1" min=0 max=10 class="validate" require>
+                            <label for="nota">Nota</label>
+                        </div>
+                    </div>
+
+                    <!-- input capa -->
+                    <div class="file-field input-field">
+                        <div class="btn purple lighten-2 black-text" >
+                            <span>Capa</span>
+                            <input type="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text">
+                        </div>
+                    </div>
+
+                    <div class="card-action">
+                        <a class="btn waves-effect waves-light grey" href="galeria.php">Cancelar</a>
+                        <a href="#" class="waves-effect waves-light btn purple">Confirmar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
---
+++ galeria.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>Clorocine</title>
+</head>
+
+<body>
+
+    <nav class="nav-extended purple lighten-3">
+        <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="cadastrar.php">Cadastrar</a></li>
+            </ul>
+        </div>
+        <div class="nav-header center">
+            <h1>CLOROCINE</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent purple darken-1">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                <li class="tab"><a href="#test2">Assistidos</a></li>
+                <li class="tab"><a href="#test3">Favoritos</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="row">
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://image.tmdb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 9,7
+                    </p>
+                    <span class="card-title">Vingadores</span>
+                    <p>Depois dos eventos devastadores de “Guerra do Infinito”, o universo está em ruínas. Com a ajuda dos aliados que restam, os Vingadores juntam-se mais uma vez para tentar desfazer as ações de Thanos e restaurar a ordem ao universo.</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://image.tmdb.org/t/p/w300/wigZBAmNrIhxp2FNGOROUAeHvdh.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 6,2
+                    </p>
+                    <span class="card-title">Ad Astra</span>
+
+                    <p>O astronauta Roy McBride viaja para lá dos limites do sistema solar para tentar encontrar o seu pai desaparecido e decifrar o mistério que ameaça a sobrevivência do planeta Terra.</p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://image.tmdb.org/t/p/w300/imcOp1kJsCsAFCoOtY5OnPrFbAf.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 9,9
+                    </p>
+                    <span class="card-title">O Auto da Compadecida</span>
+
+                    <p>As aventuras de João Grilo (Matheus Natchergaele), um sertanejo pobre e mentiroso, e Chicó (Selton Mello), o mais covarde dos homens. Ambos lutam pelo pão de cada dia e atravessam por vários episódios enganando a todos da pequena cidade em que vivem.</p>
+                </div>
+            </div>
+
+        </div>   
+        
+        <div class="col s3">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="https://image.tmdb.org/t/p/w300/lFx2i2pg1BoaD7grcpGDyHM1eML.jpg">
+                    <a class="btn-floating halfway-fab waves-effect waves-light red">
+                        <i class="material-icons">favorite_border</i>
+                    </a>
+                </div>
+                <div class="card-content">
+                    <p class="valign-wrapper">
+                        <i class="material-icons amber-text">star</i> 8,1
+                    </p>
+                    <span class="card-title">Star Wars: Episódio IX - A Ascensão de Skywalker </span>
+
+                    <p>Uma jornada épica para uma galáxia muito, muito distante. A fascinante conclusão da saga Skywalker, onde vão nascer novas lendas e a batalha final pela liberdade ainda está para chegar.</p>
+                </div>
+            </div>
+
+        </div>        
+    </div>
+
+    </div>
+
+
+
+</body>
+
+</html>
