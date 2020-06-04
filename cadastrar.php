797d9a8c2fdea3a8672c1fac7cd0988c72ff797e
João Carlos <joao@etecia.com.br>
2020-06-04 10:14:06 -0300

    ajustes layout

--- cadastrar.php
+++ cadastrar.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <title>Clorocine</title>
-</head>
+<?php include "cabecalho.php" ?>
 
 <body>
 
@@ -20,18 +6,11 @@
         <div class="nav-wrapper">
             <ul id="nav-mobile" class="right">
                 <li><a href="galeria.php">Galeria</a></li>
-                <li><a href="cadastrar.php">Cadastrar</a></li>
+                <li class="active"><a href="cadastrar.php">Cadastrar</a></li>
             </ul>
         </div>
         <div class="nav-header center">
             <h1>CLOROCINE</h1>
-        </div>
-        <div class="nav-content">
-            <ul class="tabs tabs-transparent purple darken-1">
-                <li class="tab"><a class="active" href="#test1">Todos</a></li>
-                <li class="tab"><a href="#test2">Assistidos</a></li>
-                <li class="tab"><a href="#test3">Favoritos</a></li>
-            </ul>
         </div>
     </nav>
 
