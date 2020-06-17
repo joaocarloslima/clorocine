24c988051f1571aa7bc194fdf36141cbd6478fff
João Carlos <joao@etecia.com.br>
2020-06-16 21:24:50 -0300

    cadastro de filmes

--- bd_up.php
+++ bd_up.php
@@ -13,7 +13,7 @@ $sql = "CREATE TABLE filmes (
         titulo VARCHAR(200) NOT NULL,
         poster VARCHAR (200),
         sinopse TEXT,
-        nota DECIMAL(2,1)
+        nota DECIMAL(3,1)
     )
 ";
 
@@ -23,8 +23,8 @@ else
     echo "\nerro ao criar tabela filmes\n"; 
 
 
-$sql = "INSERT INTO filmes (id, titulo, poster, sinopse, nota) VALUES (
-        0,
+$sql = "INSERT INTO filmes (titulo, poster, sinopse, nota) VALUES (
+        
         'Vingadores',
         'https://image.tmdb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg',
         'Depois dos eventos devastadores de “Guerra do Infinito”, o universo está em ruínas. Com a ajuda dos aliados que restam, os Vingadores juntam-se mais uma vez para tentar desfazer as ações de Thanos e restaurar a ordem ao universo',
@@ -49,3 +49,13 @@ if ($bd->exec($sql))
 echo "\nfilmes inseridos com sucesso\n"; 
 else 
 echo "\nerro ao inserir filmes\n";
+
+
+
+$sql = "INSERT INTO filmes (id, titulo, poster, sinopse, nota) VALUES ( 2, 'Thor', 'thos.jpg', 'um filme bem nosta', 7)";
+
+if ($bd->exec($sql)) 
+echo "\nfilmes inseridos com sucesso\n"; 
+else 
+echo "\nerro ao inserir filmes\n";
+
