83b5e19eda6123a01f37e3728b3d5f156669b6b9
João Carlos <joao@etecia.com.br>
2020-06-16 21:25:45 -0300

    cadastro de filmes sqlite

--- cadastrar.php
+++ cadastrar.php
@@ -2,67 +2,69 @@
 
 <body>
 
-    <nav class="nav-extended purple lighten-3">
-        <div class="nav-wrapper">
-            <ul id="nav-mobile" class="right">
-                <li><a href="galeria.php">Galeria</a></li>
-                <li class="active"><a href="cadastrar.php">Cadastrar</a></li>
-            </ul>
-        </div>
-        <div class="nav-header center">
-            <h1>CLOROCINE</h1>
-        </div>
-    </nav>
+  <nav class="nav-extended purple lighten-3">
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="right">
+        <li><a href="galeria.php">Galeria</a></li>
+        <li class="active"><a href="cadastrar.php">Cadastrar</a></li>
+      </ul>
+    </div>
+    <div class="nav-header center">
+      <h1>CLOROCINE</h1>
+    </div>
+  </nav>
 
-    <div class="row">
-        <div class="col s6 offset-s3">
-            <div class="card">
-                <div class="card-content">
-                    <span class="card-title">Cadastrar Filme</span>
+  <div class="row">
+    <form action="inserirFilme.php" method="POST">
+      <div class="col s6 offset-s3">
+        <div class="card">
+          <div class="card-content">
+            <span class="card-title">Cadastrar Filme</span>
 
-                    <!-- input titulo-->
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="titulo" type="text" class="validate" require>
-                            <label for="titulo">Título do Filme</label>
-                        </div>
-                    </div>
-
-                    <!-- input sinopse-->
-                    <div class="row">
-                        <form class="col s12">
-                            <div class="row">
-                                <div class="input-field col s12">
-                                    <textarea id="sinopse" class="materialize-textarea"></textarea>
-                                    <label for="sinopse">Sinopse</label>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-                    <!-- input nota-->
-                    <div class="row">
-                        <div class="input-field col s4">
-                            <input id="nota" type="number" step=".1" min=0 max=10 class="validate" require>
-                            <label for="nota">Nota</label>
-                        </div>
-                    </div>
-
-                    <!-- input capa -->
-                    <div class="file-field input-field">
-                        <div class="btn purple lighten-2 black-text" >
-                            <span>Capa</span>
-                            <input type="file">
-                        </div>
-                        <div class="file-path-wrapper">
-                            <input class="file-path validate" type="text">
-                        </div>
-                    </div>
-
-                    <div class="card-action">
-                        <a class="btn waves-effect waves-light grey" href="galeria.php">Cancelar</a>
-                        <a href="#" class="waves-effect waves-light btn purple">Confirmar</a>
-                    </div>
-                </div>
+            <!-- input titulo-->
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="titulo" type="text" class="validate" name="titulo" required>
+                <label for="titulo">Título do Filme</label>
+              </div>
             </div>
+
+            <!-- input sinopse-->
+            <div class="row">
+              <div class="row">
+                <div class="input-field col s12">
+                  <textarea name="sinopse" id="sinopse" class="materialize-textarea"></textarea>
+                  <label for="sinopse">Sinopse</label>
+                </div>
+              </div>
+            </div>
+
+            <!-- input nota-->
+            <div class="row">
+              <div class="input-field col s4">
+                <input id="nota" name="nota" type="number" step=".1" min=0 max=10 class="validate" required>
+                <label for="nota">Nota</label>
+              </div>
+            </div>
+
+            <!-- input capa -->
+            <div class="file-field input-field">
+              <div class="btn purple lighten-2 black-text">
+                <span>Capa</span>
+                <input type="file">
+              </div>
+              <div class="file-path-wrapper">
+                <input class="file-path validate" type="text" name="poster">
+              </div>
+            </div>
+
+            <div class="card-action">
+              <a class="btn waves-effect waves-light grey" href="galeria.php">Cancelar</a>
+              <button type="submit" class="waves-effect waves-light btn purple">Confirmar</button>
+            </div>
+          </div>
         </div>
+      </div>
+    </form>
+  </div>
+</body>
--- galeria.php
+++ galeria.php
@@ -6,89 +6,68 @@ $bd = new SQLite3("filmes.db");
 $sql = "SELECT * FROM filmes";
 $filmes = $bd->query($sql);
 
-$filme1 = [
-    "titulo" => "Vingadores",
-    "nota" => 9.9,
-    "sinopse" => "Depois dos eventos devastadores de “Guerra do Infinito”, o universo está em ruínas. Com a ajuda dos aliados que restam, os Vingadores juntam-se mais uma vez para tentar desfazer as ações de Thanos e restaurar a ordem ao universo.",
-    "poster" => "https://image.tmdb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
-];
-
-$filme2 = [
-    "titulo" => "Ad Astra",
-    "nota" => 6.2,
-    "sinopse" => "O astronauta Roy McBride viaja para lá dos limites do sistema solar para tentar encontrar o seu pai desaparecido e decifrar o mistério que ameaça a sobrevivência do planeta Terra.",
-    "poster" => "https://image.tmdb.org/t/p/w300/wigZBAmNrIhxp2FNGOROUAeHvdh.jpg"
-];
-
-$filme3 = [
-    "titulo" => "O auto da compadecida",
-    "nota" => 10.0,
-    "sinopse" => "As aventuras de João Grilo (Matheus Natchergaele), um sertanejo pobre e mentiroso, e Chicó (Selton Mello), o mais covarde dos homens. Ambos lutam pelo pão de cada dia e atravessam por vários episódios enganando a todos da pequena cidade em que vivem.",
-    "poster" => "https://image.tmdb.org/t/p/w300/imcOp1kJsCsAFCoOtY5OnPrFbAf.jpg"
-];
-
-$filme4 = [
-    "titulo" => "Star Wars: Episódio IX - A Ascensão de Skywalker",
-    "nota" => 8.1,
-    "sinopse" => "Uma jornada épica para uma galáxia muito, muito distante. A fascinante conclusão da saga Skywalker, onde vão nascer novas lendas e a batalha final pela liberdade ainda está para chegar.",
-    "poster" => "https://image.tmdb.org/t/p/w300/lFx2i2pg1BoaD7grcpGDyHM1eML.jpg"
-];
-
-//$filmes = [$filme1, $filme2, $filme3, $filme4];
-
-
 ?>
 
 <body>
 
-    <nav class="nav-extended purple lighten-3">
-        <div class="nav-wrapper">
-            <ul id="nav-mobile" class="right">
-                <li class="active"><a href="galeria.php">Galeria</a></li>
-                <li><a href="cadastrar.php">Cadastrar</a></li>
-            </ul>
-        </div>
-        <div class="nav-header center">
-            <h1>CLOROCINE</h1>
-        </div>
-        <div class="nav-content">
-            <ul class="tabs tabs-transparent purple darken-1">
-                <li class="tab"><a class="active" href="#test1">Todos</a></li>
-                <li class="tab"><a href="#test2">Assistidos</a></li>
-                <li class="tab"><a href="#test3">Favoritos</a></li>
-            </ul>
-        </div>
-    </nav>
+  <nav class="nav-extended purple lighten-3">
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="right">
+        <li class="active"><a href="galeria.php">Galeria</a></li>
+        <li><a href="cadastrar.php">Cadastrar</a></li>
+      </ul>
+    </div>
+    <div class="nav-header center">
+      <h1>CLOROCINE</h1>
+    </div>
+    <div class="nav-content">
+      <ul class="tabs tabs-transparent purple darken-1">
+        <li class="tab"><a class="active" href="#test1">Todos</a></li>
+        <li class="tab"><a href="#test2">Assistidos</a></li>
+        <li class="tab"><a href="#test3">Favoritos</a></li>
+      </ul>
+    </div>
+  </nav>
 
+  <div class="container">
     <div class="row">
 
-    <?php while ($filme = $filmes->fetchArray()) :?>
-    <div class="col s3">
-            <div class="card hoverable">
-                <div class="card-image">
-                    <img src="<?= $filme["poster"] ?>">
-                    <a class="btn-floating halfway-fab waves-effect waves-light red">
-                        <i class="material-icons">favorite_border</i>
-                        <?= $filme["nota"] ?>
-                    </a>
-                </div>
-                <div class="card-content">
-                    <p class="valign-wrapper">
-                        <i class="material-icons amber-text">star</i> 8,1
-                    </p>
-                    <span class="card-title"><?= $filme["titulo"] ?></span>
-
-                    <p><?= $filme["sinopse"] ?></p>
-                </div>
+      <?php while ($filme = $filmes->fetchArray()) : ?>
+        <div class="col s12 m6 l3">
+          <div class="card hoverable">
+            <div class="card-image">
+              <img src="<?= $filme["poster"] ?>">
+              <a class="btn-floating halfway-fab waves-effect waves-light red">
+                <i class="material-icons">favorite_border</i>
+              </a>
             </div>
-        </div>   
-        <?php endwhile ?>     
+            <div class="card-content">
+              <p class="valign-wrapper">
+                <i class="material-icons amber-text">star</i> <?= $filme["nota"] ?>
+              </p>
+              <span class="card-title"><?= $filme["titulo"] ?></span>
+
+              <p><?= $filme["sinopse"] ?></p>
+            </div>
+          </div>
+        </div>
+      <?php endwhile ?>
     </div>
 
-    </div>
+  </div>
 
+
+  </div>
 
 
 </body>
+
+<?php if( isset($_GET["msg"])): ?>
+  <script>
+    M.toast({
+      html: '<?= $_GET["msg"] ?>'
+    });
+  </script>
+<?php endif ?>
 
 </html>
